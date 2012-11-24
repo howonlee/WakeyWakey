54d4a3ddab4e14a4e7fd7b734924566e9231ba77
Howon Lee <hlee.howon@gmail.com>
2012-11-23 22:13:24 -0800

    added scaffolding for training stuff

--- wakeywakey.py
+++ wakeywakey.py
@@ -14,11 +14,15 @@ def draw_rects(img, rects, color):
     for x1, y1, x2, y2 in rects:
         cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
+def avg(l):
+    return sum(l) / len(l)
+
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
     cascade = cv2.CascadeClassifier("./haardetectors/haarcascade_frontalface_alt.xml")
     nested = cv2.CascadeClassifier("./haardetectors/haarcascade_eye.xml")
-    counter = 0 #counter for training on our own stuff
+    counter1 = 0 #counter for taking new img
+    counter2 = 0
     facenum = 0 #index for training face
     eyenum = 0 #index for training eyes
 
@@ -33,17 +37,25 @@ if __name__ == "__main__":
             roi = gray[y1:y2, x1:x2]
             vis_roi = vis[y1:y2, x1:x2]
             subrects = detect(roi.copy(), nested)
-            if (counter > 20):
-                counter = 0
+            if (counter1 > 20):
+                counter1 = 0
                 facenum += 1
-                print "walla"
+                print "saving images"
                 cv2.imwrite('trainface' + str(facenum) + '.jpg', roi)
+                eyepos = []
                 for x3, y3, x4, y4 in subrects:
                     eyenum += 1
                     sub_roi = roi[y3:y4, x3:x4]
+                    pos = [avg([x3, x4]), avg([y3, y4])]
+                    eyepos.append(pos)
                     cv2.imwrite('traineye' + str(eyenum) + '.jpg', sub_roi)
+            if (counter2 > 200):
+                counter2 = 0
+                #now train this crap
+                print "training on images"
             draw_rects(vis_roi, subrects, (255, 0, 0))
-            counter += 1
+            counter1 += 1
+            counter2 += 1
         cv2.imshow('video test', vis)
         key = cv2.waitKey(10)
         if key == 27:
